da10cb12f7a149c6be48aee6ecda846a79520eed
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-04-27 02:16:03 +0300

    UserController POST

--- src/main/java/com/lazarenko/dmytro/usersapi/controller/UserController.java
+++ src/main/java/com/lazarenko/dmytro/usersapi/controller/UserController.java
@@ -2,10 +2,14 @@ package com.lazarenko.dmytro.usersapi.controller;
 
 import com.lazarenko.dmytro.usersapi.model.User;
 
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +19,9 @@ public class UserController {
 
     private final List<User> users = new ArrayList<>();
 
+    @Value("${user.minimum-age}")
+    private int minimumAge;
+
     @GetMapping
     public List<User> all() {
         return users;
@@ -23,6 +30,16 @@ public class UserController {
     @GetMapping("/{email}")
     public User one(@PathVariable String email) {
         return findUserByEmail(email);
+    }
+
+    @PostMapping
+    public User newUser(@Valid @RequestBody User user) {
+        if (LocalDate.now().minusYears(minimumAge).isBefore(user.getDateOfBirth())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User must be at least " + minimumAge + " years old");
+        }
+
+        users.add(user);
+        return user;
     }
 
     private User findUserByEmail(String email) {
