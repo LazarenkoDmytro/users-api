f4abec6972e876861bf6070213a530ef7caf5c40
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-04-27 16:47:32 +0300

    Assembler is now more informative

--- src/main/java/com/lazarenko/dmytro/usersapi/model/assembler/UserModelAssembler.java
+++ src/main/java/com/lazarenko/dmytro/usersapi/model/assembler/UserModelAssembler.java
@@ -7,6 +7,8 @@ import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -19,6 +21,9 @@ public class UserModelAssembler implements RepresentationModelAssembler<User, En
 
         return EntityModel.of(user,
                 linkTo(methodOn(UserController.class).one(user.getEmail())).withSelfRel(),
-                linkTo(methodOn(UserController.class).all()).withRel("users"));
+                linkTo(methodOn(UserController.class).all()).withRel("users"),
+                linkTo(methodOn(UserController.class).updateUser(user.getEmail(), new HashMap<>())).withRel("update"),
+                linkTo(methodOn(UserController.class).replaceUser(user.getEmail(), new User())).withRel("replace"),
+                linkTo(methodOn(UserController.class).deleteUser(user.getEmail())).withRel("delete"));
     }
 }
